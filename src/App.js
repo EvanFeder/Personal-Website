5db785ae6084bb3d1ef65779a680b63dd60f9aac
Evan Feder <evanmfeder@gmail.com>
2019-07-27 23:46:22 -0400

    Another attempt at loading json

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/Personal-Website/resumeData.json',
+      url:'/data/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
